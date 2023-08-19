9058c9fc666a1142e461438d924a8e2f5ece7d51
celikomerdev <celikomerdev@gmail.com>
2023-08-19 19:28:17 +0300

    tut 4 complete

---
+++ UnityComputeShaders - start/Assets/Tut/4/AssignTexture.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssignTexture : MonoBehaviour
+{
+	[SerializeField]private Renderer rend = null;
+	[SerializeField]private ComputeShader shader = null;
+	[SerializeField]private int texResolution = 256;
+	
+	private RenderTexture outputTexture = null;
+	private int kernelHandle = 0;
+	
+	private void Start()
+	{
+		outputTexture = new RenderTexture(texResolution,texResolution,0);
+		outputTexture.enableRandomWrite = true;
+		outputTexture.Create();
+		rend.material.SetTexture("_MainTex",outputTexture);
+		InitShader();
+		DispatchShader(texResolution/16,texResolution/16);
+	}
+	
+	private void InitShader()
+	{
+		kernelHandle = shader.FindKernel("CSMain");
+		shader.SetTexture(kernelHandle,"Result",outputTexture);
+	}
+	
+	private void DispatchShader(int x,int y)
+	{
+		shader.Dispatch(kernelHandle,x,y,1);
+	}
+	
+	
+	private void Update()
+	{
+		if(Input.GetKeyUp(KeyCode.Space)) DispatchShader(texResolution/8,texResolution/8);
+	}
+}
