02d48ef74caab977229f24332d067fdc99614856
celikomerdev <celikomerdev@gmail.com>
2023-08-20 14:03:51 +0300

    moving circles done

--- UnityComputeShaders - start/Assets/Scripts/PassData.cs
+++ UnityComputeShaders - start/Assets/Scripts/PassData.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using PBDFluid;
 
 public class PassData : MonoBehaviour
 {
 	public ComputeShader shader;
 	public int texResolution = 1024;
+	public int count = 4;
 	
 	private Renderer rend;
 	private RenderTexture outputTexture;
@@ -15,6 +17,15 @@ public class PassData : MonoBehaviour
 	public Color clearColor = new Color();
 	public Color circleColor = new Color();
 	
+	private ComputeBuffer buffer;
+	private Circle[] circleData;
+	struct Circle
+	{
+		public float radius;
+		public Vector2 origin;
+		public Vector2 velocity;
+	}
+	
 	private void Start()
 	{
 		outputTexture = new RenderTexture(texResolution, texResolution, 0);
@@ -22,7 +33,29 @@ public class PassData : MonoBehaviour
 		outputTexture.Create();
 		rend = GetComponent<Renderer>();
 		rend.enabled = true;
+		InitData();
 		InitShader();
+	}
+	
+	private void InitData()
+	{
+		circlesHandle = shader.FindKernel("Circles");
+		uint threadGroupSizeX = 1;
+		shader.GetKernelThreadGroupSizes(circlesHandle,out threadGroupSizeX,out _,out _);
+		int total = (int)threadGroupSizeX*count;
+		
+		circleData = new Circle[total];
+		for (int i = 0; i < circleData.Length; i++)
+		{
+			Circle circle = circleData[i];
+			circle.origin.x = Random.Range(0,texResolution);
+			circle.origin.y = Random.Range(0,texResolution);
+			circle.velocity.x = Random.Range(-100f,100f);
+			circle.velocity.y = Random.Range(-100f,100f);
+			circle.radius = Random.Range(10f,30f);
+			circleData[i] = circle;
+		}
+		
 	}
 	
 	private void InitShader()
@@ -34,6 +67,12 @@ public class PassData : MonoBehaviour
 		circlesHandle = shader.FindKernel("Circles");
 		shader.SetTexture(clearHandle,"Result",outputTexture);
 		shader.SetTexture(circlesHandle,"Result",outputTexture);
+		
+		
+		int stride = sizeof(float)*5;
+		buffer = new ComputeBuffer(circleData.Length,stride);
+		buffer.SetData(circleData);
+		shader.SetBuffer(circlesHandle,"circlesBuffer",buffer);
 	}
 	
 	private void DispatchKernel(int count)
@@ -44,9 +83,9 @@ public class PassData : MonoBehaviour
 	
 	private void Update()
 	{
-		shader.SetFloat("time",Time.time*0.001f);
+		shader.SetFloat("time",Time.time);
 		shader.SetVector(nameof(clearColor),clearColor);
 		shader.SetVector(nameof(circleColor),circleColor);
-		DispatchKernel(4);
+		DispatchKernel(count);
 	}
 }
