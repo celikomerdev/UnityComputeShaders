309b8f906922d7636d76516126ee82bf5059449d
celikomerdev <celikomerdev@gmail.com>
2023-08-26 22:35:55 +0300

    mesh deform completed

--- UnityComputeShaders - start/Assets/Scripts/MeshDeform.cs
+++ UnityComputeShaders - start/Assets/Scripts/MeshDeform.cs
@@ -1,82 +1,81 @@
 ﻿using UnityEngine;
-using System.Collections;
-
 
 public class MeshDeform : MonoBehaviour
 {
-    public ComputeShader shader;
-    [Range(0.5f, 2.0f)]
-	public float radius;
+	private void Start()
+	{
+		InitVertexArrays();
+		InitGPUBuffers();
+	}
 	
-    int kernelHandle;
-    Mesh mesh;
-    
-    // Use this for initialization
-    void Start()
-    {
-    
-        if (InitData())
-        {
-            InitShader();
-        }
-    }
-
-    private bool InitData()
-    {
-        kernelHandle = shader.FindKernel("CSMain");
-
-        MeshFilter mf = GetComponent<MeshFilter>();
-
-        if (mf == null)
-        {
-            Debug.Log("No MeshFilter found");
-            return false;
-        }
-
-        InitVertexArrays(mf.mesh);
-        InitGPUBuffers();
-
-        mesh = mf.mesh;
-
-        return true;
-    }
-
-    private void InitShader()
-    {
-        shader.SetFloat("radius", radius);
-
-    }
-    
-    private void InitVertexArrays(Mesh mesh)
-    {
-        
-    }
-
-    private void InitGPUBuffers()
-    {
-        
-    }
-    
-    void GetVerticesFromGPU()
-    {
-        
-    }
-
-    void Update(){
-        if (shader)
-        {
-        	shader.SetFloat("radius", radius);
-            float delta = (Mathf.Sin(Time.time) + 1)/ 2;
-            shader.SetFloat("delta", delta);
-            shader.Dispatch(kernelHandle, 1, 1, 1);
-            
-            GetVerticesFromGPU();
-        }
-    }
-
-    void OnDestroy()
-    {
-        
-    }
+	[Range(0.5f,2.0f)]
+	[SerializeField]private float radius = 1f;
+	private void Update()
+	{
+		shader.SetFloat("radius", radius);
+		float delta = (Mathf.Sin(Time.time) + 1)/ 2;
+		shader.SetFloat("delta", delta);
+		shader.Dispatch(kernelMeshDeform,threadGroupsX:mesh.vertexCount,1,1);
+		GetVerticesFromGPU();
+	}
+	
+	
+	private Mesh mesh = null;
+	private Vertex[] vertexOriginal = new Vertex[0];
+	private Vertex[] vertexModified = new Vertex[0];
+	private void InitVertexArrays()
+	{
+		mesh = GetComponent<MeshFilter>().mesh;
+		vertexOriginal = new Vertex[mesh.vertexCount];
+		vertexModified = new Vertex[mesh.vertexCount];
+		for (int i = 0; i < mesh.vertexCount; i++)
+		{
+			Vertex vert = new Vertex(mesh.vertices[i],mesh.normals[i]);
+			vertexOriginal[i] = vert;
+			vertexModified[i] = vert;
+		}
+	}
+	
+	[SerializeField]private ComputeShader shader = null;
+	private int kernelMeshDeform = -1;
+	private ComputeBuffer bufferOriginal = null;
+	private ComputeBuffer bufferModified = null;
+	private void InitGPUBuffers()
+	{
+		kernelMeshDeform = shader.FindKernel("MeshDeform");
+		bufferOriginal = new ComputeBuffer(mesh.vertexCount,6*sizeof(float));
+		bufferModified = new ComputeBuffer(mesh.vertexCount,6*sizeof(float));
+		
+		bufferOriginal.SetData(vertexOriginal);
+		bufferModified.SetData(vertexModified);
+		
+		shader.SetBuffer(kernelMeshDeform,"bufferOriginal",bufferOriginal);
+		shader.SetBuffer(kernelMeshDeform,"bufferModified",bufferModified);
+	}
+	
+	private void GetVerticesFromGPU()
+	{
+		bufferModified.GetData(vertexModified);
+		Vector3[] returnedPositions = new Vector3[mesh.vertexCount];
+		Vector3[] returnedNormals = new Vector3[mesh.vertexCount];
+		for (int i = 0; i < mesh.vertexCount; i++)
+		{
+			returnedPositions[i] = vertexModified[i].position;
+			returnedNormals[i] = vertexModified[i].normal;
+		}
+		mesh.SetVertices(returnedPositions);
+		mesh.SetNormals(returnedNormals);
+	}
+	
+	
+	private struct Vertex
+	{
+		public Vector3 position;
+		public Vector3 normal;
+		public Vertex(Vector3 position,Vector3 normal)
+		{
+			this.position = position;
+			this.normal = normal;
+		}
+	}
 }
-
