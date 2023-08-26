4112179308c8360cfb162fa6112dfd280321c68c
celikomerdev <celikomerdev@gmail.com>
2023-08-26 21:53:57 +0300

    noise functions

--- UnityComputeShaders - start/Assets/Scripts/ProceduralWood.cs
+++ UnityComputeShaders - start/Assets/Scripts/ProceduralWood.cs
@@ -4,65 +4,64 @@ using System.Collections;
 public class ProceduralWood : MonoBehaviour
 {
 
-    public ComputeShader shader;
-    public int texResolution = 256;
+	public ComputeShader shader;
+	public int texResolution = 256;
 
-    Renderer rend;
-    RenderTexture outputTexture;
+	Renderer rend;
+	RenderTexture outputTexture;
 
-    int kernelHandle;
+	int kernelHandle;
 
-    public Color paleColor = new Color(0.733f, 0.565f, 0.365f, 1);
-    public Color darkColor = new Color(0.49f, 0.286f, 0.043f, 1);
-    public float frequency = 2.0f;
-    public float noiseScale = 6.0f;
-    public float ringScale = 0.6f;
-    public float contrast = 4.0f;
+	public Color paleColor = new Color(0.733f, 0.565f, 0.365f, 1);
+	public Color darkColor = new Color(0.49f, 0.286f, 0.043f, 1);
+	public float frequency = 2.0f;
+	public float noiseScale = 6.0f;
+	public float ringScale = 0.6f;
+	public float contrast = 4.0f;
 
-    // Use this for initialization
-    void Start()
-    {
-        outputTexture = new RenderTexture(texResolution, texResolution, 0);
-        outputTexture.enableRandomWrite = true;
-        outputTexture.Create();
+	// Use this for initialization
+	void Start()
+	{
+		outputTexture = new RenderTexture(texResolution, texResolution, 0);
+		outputTexture.enableRandomWrite = true;
+		outputTexture.Create();
 
-        rend = GetComponent<Renderer>();
-        rend.enabled = true;
+		rend = GetComponent<Renderer>();
+		rend.enabled = true;
 
-        InitShader();
-    }
+		InitShader();
+	}
 
-    private void InitShader()
-    {
-        kernelHandle = shader.FindKernel("CSMain");
+	private void InitShader()
+	{
+		kernelHandle = shader.FindKernel("CSMain");
 
-        shader.SetInt("texResolution", texResolution);
+		shader.SetInt("texResolution", texResolution);
 
-        shader.SetVector("paleColor", paleColor);
-        shader.SetVector("darkColor", darkColor);
-        shader.SetFloat("frequency", frequency);
-        shader.SetFloat("noiseScale", noiseScale);
-        shader.SetFloat("ringScale", ringScale);
-        shader.SetFloat("contrast", contrast);
 
-        shader.SetTexture(kernelHandle, "Result", outputTexture);
 
-        rend.material.SetTexture("_MainTex", outputTexture);
+		shader.SetTexture(kernelHandle, "Result", outputTexture);
+		rend.material.SetTexture("_MainTex", outputTexture);
+		DispatchShader(texResolution / 8, texResolution / 8);
+	}
 
-        DispatchShader(texResolution / 8, texResolution / 8);
-    }
+	private void DispatchShader(int x, int y)
+	{
+		shader.Dispatch(kernelHandle, x, y, 1);
+	}
 
-    private void DispatchShader(int x, int y)
-    {
-        shader.Dispatch(kernelHandle, x, y, 1);
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.U))
-        {
-            DispatchShader(texResolution / 8, texResolution / 8);
-        }
-    }
+	void Update()
+	{
+		if (Input.GetKeyUp(KeyCode.U))
+		{
+			shader.SetVector("paleColor", paleColor);
+			shader.SetVector("darkColor", darkColor);
+			shader.SetFloat("frequency", frequency);
+			shader.SetFloat("noiseScale", noiseScale);
+			shader.SetFloat("ringScale", ringScale);
+			shader.SetFloat("contrast", contrast);
+		
+			DispatchShader(texResolution / 8, texResolution / 8);
+		}
+	}
 }
-
--- UnityComputeShaders - start/Assets/Scripts/SimpleNoise.cs
+++ UnityComputeShaders - start/Assets/Scripts/SimpleNoise.cs
@@ -1,49 +1,39 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class SimpleNoise : MonoBehaviour
 {
-
-    public ComputeShader shader;
-    public int texResolution = 256;
-
-    Renderer rend;
-    RenderTexture outputTexture;
-
-    int kernelHandle;
-
-    // Use this for initialization
-    void Start()
-    {
-        outputTexture = new RenderTexture(texResolution, texResolution, 0);
-        outputTexture.enableRandomWrite = true;
-        outputTexture.Create();
-
-        rend = GetComponent<Renderer>();
-        rend.enabled = true;
-
-        InitShader();
-    }
-
-    private void InitShader()
-    {
-        kernelHandle = shader.FindKernel("CSMain");
-
-        shader.SetInt("texResolution", texResolution);
-        shader.SetTexture(kernelHandle, "Result", outputTexture);
-
-        rend.material.SetTexture("_MainTex", outputTexture);
-    }
-
-    private void DispatchShader(int x, int y)
-    {
-        shader.SetFloat("time", Time.time);
-        shader.Dispatch(kernelHandle, x, y, 1);
-    }
-
-    void Update()
-    {
-        DispatchShader(texResolution / 8, texResolution / 8);
-    }
+	public ComputeShader shader;
+	public int texResolution = 256;
+	private Renderer rend;
+	private RenderTexture outputTexture;
+	private int kernelHandle;
+	
+	private void Start()
+	{
+		outputTexture = new RenderTexture(texResolution, texResolution, 0);
+		outputTexture.enableRandomWrite = true;
+		outputTexture.Create();
+		rend = GetComponent<Renderer>();
+		rend.enabled = true;
+		InitShader();
+	}
+	
+	private void InitShader()
+	{
+		kernelHandle = shader.FindKernel("CSMain");
+		shader.SetInt("texResolution", texResolution);
+		shader.SetTexture(kernelHandle, "Result", outputTexture);
+		rend.material.SetTexture("_MainTex", outputTexture);
+	}
+	
+	private void DispatchShader(int x, int y)
+	{
+		shader.SetFloat("time", Time.time);
+		shader.Dispatch(kernelHandle, x, y, 1);
+	}
+	
+	private void Update()
+	{
+		DispatchShader(texResolution / 8, texResolution / 8);
+	}
 }
-
